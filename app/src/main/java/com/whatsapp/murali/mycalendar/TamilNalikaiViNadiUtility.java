cd093b12dbc1f5b43c75cd131e9a2182911bda98
sureshn <sureshlearner@gmail.com>
2018-11-16 22:11:27 +0530

    time utility files added

---
+++ app/src/main/java/com/whatsapp/murali/mycalendar/TamilNalikaiViNadiUtility.java
@@ -0,0 +1,8 @@
+package com.whatsapp.murali.mycalendar;
+
+/**
+ * Created by KaGee on 11/16/2018.
+ */
+
+public class TamilNalikaiViNadiUtility {
+}
---
+++ app/src/main/java/com/whatsapp/murali/mycalendar/TamilSuntimeUtility.java
@@ -0,0 +1,25 @@
+package com.whatsapp.murali.mycalendar;
+
+import java.util.HashMap;
+
+/**
+ * Created by KaGee on 11/16/2018.
+ */
+
+public class TamilSuntimeUtility {
+
+
+    public String getSunRaiseHours()
+    {
+
+
+       return "06";
+
+    }
+
+
+    public String getSunRaiseMinutes() {
+
+        return "00";
+    }
+}
